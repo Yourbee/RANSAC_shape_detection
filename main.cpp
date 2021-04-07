1d0efd34085ce4abb8f0e9c0547a71e92cef2d99
myqiun <myqiun@gmail.com>
2021-04-07 09:09:18 +0800

    Rotate best vertical plane to calculate.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Taken from https://github.com/ihmcrobotics/ihmc-open-robotics-software/blob/5f5345ea78f681c1ca815bb1539041b5d0ab54d0/ihmc-sensor-processing/csrc/ransac_schnabel/main.cpp
+﻿// Taken from https://github.com/ihmcrobotics/ihmc-open-robotics-software/blob/5f5345ea78f681c1ca815bb1539041b5d0ab54d0/ihmc-sensor-processing/csrc/ransac_schnabel/main.cpp
 
 #include <PointCloud.h>
 #include <RansacShapeDetector.h>
@@ -6,6 +6,8 @@
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
+
+#include <opencv2/opencv.hpp>
 
 #include <iostream>
 
@@ -111,6 +113,12 @@ int showPointFlat(std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> v_cloud)
 		viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, cloud_name);
 
 	}
+
+	pcl::PointXYZ p1, p2, p3, p4;
+	p1.x = 0; p1.y = 0; p1.z = 0;
+	p2.x = 1; p2.y = 1; p2.z = 1;
+	viewer->addLine(p1, p2);
+
 	viewer->spin();
 
 	return 0;
@@ -124,6 +132,7 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	// 读取点云
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 	//std::string file = "house1.pcd";
 	//std::string file_path = "C:/Users/myqiu/Desktop/pts_data_file/house/" + file;
@@ -136,8 +145,9 @@ int main(int argc, char** argv)
 	std::uint32_t n_points = cloud->width * cloud->height;
 	cout << "Total points: " << n_points << endl;
 
-	move2XOY(cloud);
+	move2XOY(cloud);  //移动到原点
 
+	//x, y, z最大最小值，用来计算尺度
 	float min_x = std::numeric_limits<float>::max();
 	float min_y = std::numeric_limits<float>::max();
 	float min_z = std::numeric_limits<float>::max();
@@ -146,6 +156,7 @@ int main(int argc, char** argv)
 	float max_y = -std::numeric_limits<float>::max();
 	float max_z = -std::numeric_limits<float>::max();
 
+	// 转换到ransac库的格式
 	PointCloud pc;
 	pc.reserve(n_points);
 	for (auto& p : *cloud)
@@ -169,6 +180,7 @@ int main(int argc, char** argv)
 	//void calcNormals( float radius, unsigned int kNN = 20, unsigned int maxTries = 100 );
 	pc.calcNormals(.01f * pc.getScale());
 
+	//参数
 	RansacShapeDetector::Options ransacOptions;
 	ransacOptions.m_epsilon = .005f * pc.getScale(); // set distance threshold to .01f of bounding box width
 		// NOTE: Internally the distance threshold is taken as 3 * ransacOptions.m_epsilon!!!
@@ -201,8 +213,9 @@ int main(int argc, char** argv)
 			<< normal[0] << " " << normal[1] << " " << normal[2] << " "
 			<< endl;
 	}
-
 	cout << endl;
+
+	//筛选,阈值：和（0，0，1）角度， 和最小点数
 	std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> v_cloud;
 	std::vector<Eigen::Vector4f> v_normals;
 	size_t rend = 0;
@@ -234,13 +247,15 @@ int main(int argc, char** argv)
 			Vec3f dist, normal;
 			shapes[i].first->Normal(dist, &normal);
 			Eigen::Vector4f coff(normal[0], normal[1], normal[2], dist[0]);
+			if (coff[3] < 0) coff *= -1;
 			cout << "coff " << coff.transpose() << endl;
 			v_normals.push_back(coff);
 		}
 		rend += shapes[i].second;
 	}
+	showPoint(v_cloud);
 
-	//first 
+	//计算相互之间的垂直度
 	std::vector<float> loss_rect(v_cloud.size(), 0);
 	for (size_t i = 0; i < v_cloud.size(); ++i)
 	{
@@ -257,7 +272,7 @@ int main(int argc, char** argv)
 		}
 		cout << i << " " << loss_rect[i] << endl;
 	}
-	// arg loss
+	// 排序
 	std::vector<size_t> arg_loss_rect;
 	arg_loss_rect.resize(loss_rect.size());
 	iota(arg_loss_rect.begin(), arg_loss_rect.end(), 0);  //0 1 2 3 4
@@ -268,10 +283,11 @@ int main(int argc, char** argv)
 	{
 		cout << arg_loss_rect[i] << " ";
 	}
+	cout << "Min loss normal " << v_normals[arg_loss_rect[0]].head(3).transpose() << endl;
 	cout << endl;
 
-	showPoint(v_cloud);
-
+	//转到OpenCV格式
+	std::vector<cv::Point2f> imagePoints;
 	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
 	for (size_t i = 0; i < v_cloud.size(); ++i)
 	{
@@ -279,11 +295,83 @@ int main(int argc, char** argv)
 		{
 			continue;
 		}
+		for (auto& p : *v_cloud[i])
+		{
+			imagePoints.emplace_back(p.x, p.y);
+		}
 		std::string cloud_name = "Cloud " + std::to_string(i);
 		viewer->addPointCloud<pcl::PointXYZRGB>(v_cloud[i], cloud_name);
 		viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, cloud_name);
 	}
+	cout << imagePoints.size() << endl;
 	viewer->spin();
 
-	showPointFlat(v_cloud);
+	//旋转最垂直边到x轴，
+	// calculate the least rect loss plane's 2D theta to (0, -1)
+	Eigen::Vector4f l_rl = v_normals[arg_loss_rect[0]];
+	double theta_2d = acos(-l_rl[1] / sqrt(1.0 * l_rl[0] * l_rl[0] + 1.0 * l_rl[1] * l_rl[1])) / M_PI * 180.0;
+	cout << "Rotate degree " << theta_2d << endl;
+	theta_2d = l_rl[0] < 0 ? -theta_2d : theta_2d;
+	cv::Mat rotation_mat = cv::getRotationMatrix2D(cv::Point2f(0, 0), theta_2d, 1);
+	std::vector<cv::Point2f> rotated_points;
+	rotated_points.resize(imagePoints.size());
+	cv::transform(imagePoints, rotated_points, rotation_mat);
+	//拟合矩形
+	cv::RotatedRect rect = cv::minAreaRect(rotated_points);
+	cv::Point2f P[4];
+	rect.points(P);
+	//cv::Rect rect = cv::boundingRect(imagePoints);
+	//P[0].x = rect.x; P[0].y = rect.y;
+	//P[1].x = rect.x + rect.width; P[1].y = rect.y;
+	//P[2].x = rect.x + rect.width; P[2].y = rect.y + rect.height;
+	//P[3].x = rect.x; P[3].y = rect.y + rect.height;
+	//遍历 寻找x y最大最小值
+	float x_minus = 0, y_minus = 0, x_plus = 0, y_plus = 0;
+	for (auto& p : rotated_points)
+	{
+		if (x_minus > p.x) x_minus = p.x;
+		else if (x_plus < p.x) x_plus = p.x;
+
+		if (y_minus > p.y) y_minus = p.y;
+		else if (y_plus < p.y) y_plus = p.y;
+	}
+	if (x_plus - 0 < 0.1)
+	{
+		cout << "不完整房屋。" << endl;
+		assert(0);
+	}
+	cout << "面积： " << (x_plus - x_minus) * (y_plus - y_minus) << endl;
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr rotated_points_3d(new pcl::PointCloud<pcl::PointXYZRGB>);
+	rotated_points_3d->reserve(rotated_points.size());
+	for (auto& p : rotated_points)
+	{
+		//cout << "p_i " << p_i << endl;
+		pcl::PointXYZRGB point;
+		point.x = p.x; point.y = p.y; point.z = 0;
+		point.r = 255; point.g = 255; point.b = 255;
+		rotated_points_3d->push_back(point);
+	}
+
+	pcl::visualization::PCLVisualizer::Ptr viewer2(new pcl::visualization::PCLVisualizer("3D Viewer"));
+
+	viewer2->addPointCloud<pcl::PointXYZRGB>(rotated_points_3d, "rotated_points_3d");
+	viewer2->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, "rotated_points_3d");
+
+	pcl::PointXYZ p1, p2, p3, p4;
+	//p1.x = P[0].x; p1.y = P[0].y; p1.z = 0;
+	//p2.x = P[1].x; p2.y = P[1].y; p2.z = 0;
+	//p3.x = P[2].x; p3.y = P[2].y; p3.z = 0;
+	//p4.x = P[3].x; p4.y = P[3].y; p4.z = 0;
+	p1.x = x_minus; p1.y = y_minus; p1.z = 0;
+	p2.x = x_plus; p2.y = y_minus; p2.z = 0;
+	p3.x = x_plus; p3.y = y_plus; p3.z = 0;
+	p4.x = x_minus; p4.y = y_plus; p4.z = 0;
+	viewer2->addLine(p1, p2, "line1");
+	viewer2->addLine(p2, p3, "line2");
+	viewer2->addLine(p3, p4, "line3");
+	viewer2->addLine(p4, p1, "line4");
+	viewer2->addCoordinateSystem();
+
+	viewer2->spin();
 }
